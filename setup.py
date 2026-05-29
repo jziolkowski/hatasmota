a50ccc1e6456bb5077a67a1aa52001a3e1e1c125
jziolkowski <jacek@ziolkowscy.com>
2026-05-29 18:23:59 +0200

    revert version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open("README.md").read()
 
 setup(
     name="HATasmota",
-    version="0.10.2",
+    version="0.10.1",
     license="MIT",
     url="https://github.com/emontnemery/hatasmota",
     author="",
